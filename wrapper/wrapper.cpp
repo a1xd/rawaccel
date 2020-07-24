a8c51940a32f78f11879270974741b43b1438129
Jacob Palecki <JacobPalecki@gmail.com>
2020-07-23 20:50:59 -0700

    Added simple c++/cli wrapper

---
+++ wrapper/wrapper.cpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "..\common\rawaccel.hpp";
+#include "wrapper.hpp";
+using namespace rawaccel;
+using namespace System;
+
+Tuple<double, double>^ ManagedAccel::Accelerate(int x, int y, double time, double mode)
+{
+	vec2d input_vec2d = {x, y};
+	vec2d output = (*accel_instance)(input_vec2d, (accel_function::milliseconds)time, (rawaccel::mode)mode);
+
+	return gcnew Tuple<double, double>(output.x, output.y);
+}
---
+++ wrapper/wrapper.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "..\common\rawaccel.hpp";
+#include "..\common\rawaccel-userspace.hpp";
+#include <iostream>
+using namespace rawaccel;
+using namespace System;
+
+public ref class ManagedAccel
+{
+protected:
+	accel_function* accel_instance;
+public:
+	ManagedAccel(accel_function* accel)
+		: accel_instance(accel)
+	{
+	}
+
+    ManagedAccel(double mode, double offset, double accel, double lim_exp, double midpoint)
+    {
+        accel_function::args_t args{};
+
+		accel_instance = new accel_function(args);
+	}
+
+    virtual ~ManagedAccel()
+    {
+        if (accel_instance != nullptr)
+        {
+            delete accel_instance;
+        }
+    }
+    !ManagedAccel()
+    {
+        if (accel_instance != nullptr)
+        {
+            delete accel_instance;
+        }
+    }
+
+    accel_function* GetInstance()
+    {
+        return accel_instance;
+    }
+
+    Tuple<double, double>^ Accelerate(int x, int y, double time, double mode);
+};
