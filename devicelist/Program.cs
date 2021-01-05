3f12802a19f93fec99b5186b0d39c3fa2f783c6a
Tomáš Pazdiora <tomas.pazdiora@gmail.com>
2021-01-05 03:46:57 +0100

    add devicelist app

---
+++ devicelist/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Management;
+using System.Windows.Forms;
+
+namespace devicelist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(new SelectQuery("Win32_PnPEntity"));
+
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                if (obj["PNPClass"].ToString() == "Mouse" && obj["HardwareID"] != null) {
+                    String[] hwidArray = (String[])(obj["HardwareID"]);
+                    String hwid = hwidArray[0].ToString().Replace(@"\", @"\\");
+                    String caption = "(" + obj["Name"].ToString() + ") Device Hardware ID:";
+                    if (MessageBox.Show(hwid, caption, MessageBoxButtons.OKCancel) == DialogResult.Cancel) {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
