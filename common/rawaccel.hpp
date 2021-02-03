48463581ecec7667f4e52fd6c1c1180dafbafd1c
a1xd <68629610+a1xd@users.noreply.github.com>
2021-02-02 22:39:19 -0500

    increase default sens cap

--- common/rawaccel.hpp
+++ common/rawaccel.hpp
@@ -65,7 +65,7 @@ namespace rawaccel {
     /// <summary> Struct to hold clamp (min and max) details for acceleration application </summary>
     struct accel_scale_clamp {
         double lo = 0;
-        double hi = 128;
+        double hi = 1e9;
 
         /// <summary>
         /// Clamps given input to min at lo, max at hi.
