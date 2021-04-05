d948065a830dafe526e38868f23a4ca4a6ba63e4
Jacob Palecki <JacobPalecki@gmail.com>
2021-04-04 23:31:14 -0700

    Add textoption for lut text display

---
+++ grapher/Models/Options/TextOption.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace grapher.Models.Options
+{
+    public class TextOption : OptionBase
+    {
+        #region Constructors
+
+        public TextOption(Label label)
+        {
+            Label = label;
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        private Label Label { get; }
+
+        public override bool Visible
+        { 
+            get 
+            { 
+                return Label.Visible;
+            }
+        }
+
+        public override int Top
+        {
+            get
+            {
+                return Label.Top;
+            }
+            set
+            {
+                Label.Top = value;
+            }
+        }
+
+        public override int Height
+        {
+            get
+            {
+                return Label.Height;
+            }
+        }
+
+        public override int Width
+        {
+            get
+            {
+                return Label.Width;
+            }
+            set
+            {
+                Label.Width = value;
+            }
+        }
+
+        public override int Left
+        {
+            get
+            {
+                return Label.Left;
+            }
+            set
+            {
+                Label.Left = value;
+            }
+        }
+
+        public override void Hide()
+        {
+            Label.Hide();
+        }
+
+        public override void Show(string Name)
+        {
+            Label.Show();
+            Label.Text = Name;
+        }
+
+        public override void AlignActiveValues()
+        {
+            // Nothing to do here
+        }
+
+        #endregion Properties
+    }
+}
