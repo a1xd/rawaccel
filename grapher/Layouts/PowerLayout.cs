d2ce37af853b402ec80fb4008b04c7fdd1c63d5e
a1xd <68629610+a1xd@users.noreply.github.com>
2021-09-07 05:09:31 -0400

    show power mode cap field

--- grapher/Layouts/PowerLayout.cs
+++ grapher/Layouts/PowerLayout.cs
@@ -15,7 +15,7 @@
             GrowthRateLayout = new OptionLayout(false, string.Empty);
             SmoothLayout = new OptionLayout(false, string.Empty);
             ScaleLayout = new OptionLayout(true, Scale);
-            CapLayout = new OptionLayout(false, string.Empty);
+            CapLayout = new OptionLayout(true, Cap);
             WeightLayout = new OptionLayout(true, Weight);
             OffsetLayout = new OptionLayout(false, string.Empty);
             LimitLayout = new OptionLayout(false, string.Empty);
