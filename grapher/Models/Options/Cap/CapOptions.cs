c58d766bb2ad0e05cef4afa123efe878cef271f9
Jacob Palecki <JacobPalecki@gmail.com>
2021-09-13 00:40:57 -0700

    Most of Cap Type options in GUI

---
+++ grapher/Models/Options/Cap/CapOptions.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace grapher.Models.Options.Cap
+{
+    public class CapOptions : OptionBase
+    {
+        public enum CapType
+        {
+            In,
+            Out,
+            Both,
+        }
+
+        public CapOptions(
+            ComboBox capTypeDropDown,
+            Option capIn,
+            Option capOut,
+            Option slope)
+        {
+            CapTypeDropdown = capTypeDropDown;
+            In = capIn;
+            Out = capOut;
+            Slope = slope;
+
+            SetupCapTypeDropdown(CapTypeDropdown);
+            CapTypeDropdown.SelectedItem = CapType.In;
+        }
+
+        public ComboBox CapTypeDropdown { get; }
+
+        public Option In { get; }
+
+        public Option Out { get; }
+
+        public Option Slope { get; }
+
+        public CapType SelectedCapType { get; private set; }
+
+        public override int Left
+        {
+            get => In.Left;
+
+            set
+            {
+                In.Left = value;
+                Out.Left = value;
+                Slope.Left = value;
+            }
+        }
+
+        public override int Top
+        {
+            get => CapTypeDropdown.Top;
+            set
+            {
+                CapTypeDropdown.Top = value;
+                Layout();
+            }
+        }
+
+        public override int Height
+        {
+            get => BottomElement.Top + BottomElement.Height - CapTypeDropdown.Top;
+        }
+
+        public override int Width
+        {
+            get => CapTypeDropdown.Width;
+
+            set
+            {
+                CapTypeDropdown.Width = value;
+                In.Width = value;
+                Out.Width = value;
+                Slope.Width = value;
+            }
+        }
+
+        public override bool Visible
+        {
+            get => CapTypeDropdown.Visible;
+        }
+
+        private Option BottomElement { get; set; }
+
+        public void Layout()
+        {
+            Layout(CapTypeDropdown.Top + CapTypeDropdown.Height + Constants.OptionVerticalSeperation);
+        }
+
+        private void Layout(int top)
+        {
+            switch (SelectedCapType)
+            {
+                case CapType.In:
+                    Slope.Show();
+                    In.Show();
+                    Out.Hide();
+
+                    Slope.Top = top;
+                    In.SnapTo(Slope);
+                    BottomElement = In;
+                    break;
+                case CapType.Out:
+                    Slope.Show();
+                    In.Hide();
+                    Out.Show();
+
+                    Slope.Top = top;
+                    In.SnapTo(Slope);
+                    BottomElement = In;
+                    break;
+                case CapType.Both:
+                    Slope.Hide();
+                    In.Show();
+                    Out.Show();
+
+                    In.Top = top;
+                    Out.SnapTo(In);
+                    BottomElement = Out;
+                    break;
+            }
+        }
+
+        private void FindSelectedTypeFromDropdown()
+        {
+            SelectedCapType = (CapType)CapTypeDropdown.SelectedItem;
+        }
+
+        private void OnCapTypeDropdownSelectedItemChanged(object sender, EventArgs e)
+        {
+            FindSelectedTypeFromDropdown();
+            Layout();
+        }
+
+        private void SetupCapTypeDropdown(ComboBox capTypeDropDown)
+        {
+            capTypeDropDown.Items.Clear();
+            capTypeDropDown.DataSource = Enum.GetValues(typeof(CapType));
+        }
+    }
+}
---
+++ grapher/Models/Options/Cap/CapTypeOptions.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace grapher.Models.Options.Cap
+{
+    public class CapTypeOptions : ComboBoxOptionsBase
+    {
+        #region Enum
+
+        public enum CapType
+        {
+            In,
+            Out,
+            Both,
+        }
+
+        #endregion Enum
+
+        #region Classes
+
+        public class CapTypeOption
+        {
+            public CapType Type { get; set; }
+
+            public string Name => Type.ToString();
+
+            public override string ToString() => Name;
+        }
+
+        #endregion Classes
+
+        #region Static
+
+        public static readonly CapTypeOption InCap = new CapTypeOption
+        {
+            Type = CapType.In,
+        };
+
+        public static readonly CapTypeOption OutCap = new CapTypeOption
+        {
+            Type = CapType.Out,
+        };
+
+        public static readonly CapTypeOption BothCap = new CapTypeOption
+        {
+            Type = CapType.Both,
+        };
+
+        public static readonly CapTypeOption[] AllCapTypeOptions = new CapTypeOption[]
+        {
+            InCap,
+            OutCap,
+            BothCap
+        };
+
+        #endregion Static
+
+        #region Constructors
+
+        public CapTypeOptions(
+            Label label,
+            ComboBox dropdown,
+            ActiveValueLabel activeValueLabel)
+            : base(
+                  label,
+                  dropdown,
+                  activeValueLabel)
+        {
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        CapTypeOption CapOption
+        {
+            get
+            {
+                return OptionsDropdown.SelectedItem as CapTypeOption;
+            }
+            set
+            {
+                OptionsDropdown.SelectedItem = value;
+            }
+        }
+
+        #endregion Properties
+    }
+}
