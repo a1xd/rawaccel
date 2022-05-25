7c77468c1eed3cbea788b8ab20c0874fafdb8ca0
Ruud Schouten <ruud.schouten@fourtress.nl>
2022-05-25 13:19:26 +0200

    Fixed naming convention

--- grapher/Models/Theming/IO/XmlColor.cs
+++ grapher/Models/Theming/IO/XmlColor.cs
@@ -6,7 +6,7 @@ namespace grapher.Models.Theming.IO
 {
     public class XmlColor
     {
-        private Color color_ = Color.Black;
+        private Color _color = Color.Black;
 
         public XmlColor()
         {
@@ -14,18 +14,18 @@ namespace grapher.Models.Theming.IO
 
         public XmlColor(Color c)
         {
-            color_ = c;
+            _color = c;
         }
 
 
         public Color ToColor()
         {
-            return color_;
+            return _color;
         }
 
         public void FromColor(Color c)
         {
-            color_ = c;
+            _color = c;
         }
 
         public static implicit operator Color(XmlColor x)
@@ -41,19 +41,19 @@ namespace grapher.Models.Theming.IO
         [XmlAttribute]
         public string Web
         {
-            get { return ColorTranslator.ToHtml(color_); }
+            get => ColorTranslator.ToHtml(_color);
             set
             {
                 try
                 {
                     if (Alpha == 0xFF) // preserve named color value if possible
-                        color_ = ColorTranslator.FromHtml(value);
+                        _color = ColorTranslator.FromHtml(value);
                     else
-                        color_ = Color.FromArgb(Alpha, ColorTranslator.FromHtml(value));
+                        _color = Color.FromArgb(Alpha, ColorTranslator.FromHtml(value));
                 }
                 catch (Exception)
                 {
-                    color_ = Color.Black;
+                    _color = Color.Black;
                 }
             }
         }
@@ -61,11 +61,11 @@ namespace grapher.Models.Theming.IO
         [XmlAttribute]
         public byte Alpha
         {
-            get { return color_.A; }
+            get => _color.A;
             set
             {
-                if (value != color_.A) // avoid hammering named color if no alpha change
-                    color_ = Color.FromArgb(value, color_);
+                if (value != _color.A) // avoid hammering named color if no alpha change
+                    _color = Color.FromArgb(value, _color);
             }
         }
 
