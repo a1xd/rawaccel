c3349cfb61d6990ccb2ca704d20a8bed4fdc5c5b
zdhenard42 <104698652+zdhenard42@users.noreply.github.com>
2022-09-03 18:32:47 -0500

    Add files via upload
    Script will add rawaccel to startup, feel free to rename

---
+++ rawaccelstartup.bat
@@ -0,0 +1,14 @@
+@echo off
+
+for /f "delims=" %%a in ('dir /b /s rawaccel.exe') do @set rawlocation=%%a
+
+set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+echo sLinkFile = "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\rawaccel.lnk" >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath = "%rawlocation%" >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+
+cscript /nologo %SCRIPT%
+del %SCRIPT%
