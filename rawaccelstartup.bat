36035233c562807a2d065dbb70d15df3e5ddd19d
zdhenard42 <104698652+zdhenard42@users.noreply.github.com>
2022-09-08 10:29:36 -0500

    Update rawaccelstartup.bat
    This is much more efficient and actually loads the users settings.
    First line with only one '>' will delete previous files created.

--- rawaccelstartup.bat
+++ rawaccelstartup.bat
@@ -1,15 +1,6 @@
 @echo off
 
-del "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\rawaccel*" 
-for /f "delims=" %%a in ('dir /b /s rawaccel.exe') do @set rawlocation=%%a
-
-set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
-
-echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
-echo sLinkFile = "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\rawaccel.lnk" >> %SCRIPT%
-echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
-echo oLink.TargetPath = "%rawlocation%" >> %SCRIPT%
-echo oLink.Save >> %SCRIPT%
-
-cscript /nologo %SCRIPT%
-del %SCRIPT%
+set path=%~dp0
+echo @echo off > "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\rawaccel.bat"
+echo cd "%path%" >> "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\rawaccel.bat"
+echo rawaccel.exe >> "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\rawaccel.bat"
