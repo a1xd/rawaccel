b31befadd6deab063e76e8426a35cf203c9cb1fe
0xFurtiv <84947139+0xFurtiv@users.noreply.github.com>
2024-07-10 13:16:02 -0700

    Add shortcut installer

---
+++ install-shortcut.ps1
@@ -0,0 +1,13 @@
+# Run as administrator
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    Start-Process -WorkingDirectory $PWD -FilePath PowerShell.exe -Verb Runas -ArgumentList $(('-File "{0}"' -f $MyInvocation.MyCommand.Path); $MyInvocation.UnboundArguments)
+    Exit
+}
+
+$rawaccel_exe = Join-Path $PSScriptRoot rawaccel.exe
+
+$shell = New-Object -comObject WScript.Shell
+$shortcut = $shell.CreateShortcut('C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Raw Accel.lnk')
+$shortcut.TargetPath = $rawaccel_exe
+$Shortcut.WorkingDirectory = '%LOCALAPPDATA%\Raw Accel'
+$shortcut.Save()
